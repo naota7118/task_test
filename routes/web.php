e1a7acf782486f65a8eb08cb91907617b125c62a
naota7118 <naota7118@gmail.com>
2021-01-05 00:51:18 +0900

    npm install

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,19 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', function () {
+  return view('welcome');
+});
 
+Route::get('tests/test', 'TestController@index');
+
+Route::group(['prefix' => 'contact', 'middleware' => 'auth'], function(){
+  Route::get('index', 'ContactFormController@index');
+});
+
+// REST
+// Route::resource('contacts', 'ContactFormController');
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
