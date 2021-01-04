1c293f2e4fcad2938b9aaafa82c335381c2bcf27
naota7118 <naota7118@gmail.com>
2021-01-05 02:02:59 +0900

    add store action

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,11 @@ Route::get('/', function () {
 
 Route::get('tests/test', 'TestController@index');
 
+// 認証が必要な場合は、'middleware' => 'auth'とよく使うので覚えておいたほうがいい
 Route::group(['prefix' => 'contact', 'middleware' => 'auth'], function(){
-  Route::get('index', 'ContactFormController@index');
+  Route::get('index', 'ContactFormController@index')->name('contact.index');;
+  Route::get('create', 'ContactFormController@create')->name('contact.create');;
+  Route::post('store', 'ContactFormController@store')->name('contact.store');;
 });
 
 // REST
